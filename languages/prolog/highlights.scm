dbe6a0af7a133587f319b6595b66b41bc7ed6b59
Sean Ryan <piefayth@gmail.com>
2024-10-05 17:50:34 -0500

    init commit

--- languages/prolog/highlights.scm
+++ languages/prolog/highlights.scm
@@ -44,3 +44,30 @@
 (double_quoted_list_notation) @string
 
 (variable_term) @variable
+
+[
+  (directive_term)
+  (clause_term)
+  (arg_list)
+  (list_notation)
+] @fold
+
+(directive_term) @indent.zero
+
+(clause_term) @indent.zero
+
+(functional_notation
+  (atom)
+  (open_ct) @indent.begin
+  (close) @indent.end)
+
+(list_notation
+  (open_list) @indent.begin
+  (close_list) @indent.end)
+
+(curly_bracketed_notation
+  (open_curly) @indent.begin
+  (close_curly) @indent.end)
+
+((comment) @injection.content
+  (#set! injection.language "comment"))
