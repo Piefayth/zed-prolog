46d49d4c28fd19ed1ee69fddbb99feca0dadce16
Sean Ryan <piefayth@gmail.com>
2024-10-05 17:50:34 -0500

    init commit

--- languages/prolog/highlights.scm
+++ languages/prolog/highlights.scm
@@ -1,4 +1,4 @@
-(comment) @comment @spell
+(comment) @comment
 
 (atom) @constant
 
@@ -11,36 +11,75 @@
 (functional_notation
   function: (atom) @function.call)
 
-(integer) @number
+[
+  (integer)
+  (negative_number
+    (integer))
+] @number
 
-(float_number) @number.float
+[
+  (float_number)
+  (negative_number
+    (float_number))
+] @number.float
 
-(directive_head) @operator
-
-(operator_notation
-  operator: _ @operator)
+[
+  (operator_1200xfx)
+  (operator_1200fx)
+  (operator_1100xfy)
+  (operator_1050xfy)
+  (operator_1000xfy)
+  (operator_900fy)
+  (operator_700xfx)
+  (operator_500yfx)
+  (operator_400yfx)
+  (operator_200xfx)
+  (operator_200xfy)
+  (operator_200fy)
+] @operator
 
 [
  (open)
  (open_ct)
  (close)
  (open_list)
- "|"
  (close_list)
  (open_curly)
  (close_curly)
 ] @punctuation.bracket
 
-[
- (arg_list_separator)
- (comma)
- (end)
- (list_notation_separator)
-] @punctuation.delimiter
+(arg_list_separator) @punctuation.delimiter
 
-(operator_notation
-  operator: (semicolon) @punctuation.delimiter)
+(list_notation_separator) @punctuation.delimiter
+
+(end) @punctuation.delimiter
 
 (double_quoted_list_notation) @string
 
 (variable_term) @variable
+
+
+(directive_term) @indent.zero
+
+(clause_term) @indent.zero
+
+(functional_notation
+  (atom)
+  (open_ct) @indent.begin
+  (close) @indent.end)
+
+(list_notation
+  (open_list) @indent.begin
+  (close_list) @indent.end)
+
+(curly_bracketed_notation
+  (open_curly) @indent.begin
+  (close_curly) @indent.end)
+
+
+[
+  (directive_term)
+  (clause_term)
+  (arg_list)
+  (list_notation)
+] @fold
